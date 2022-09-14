f2bc016e32228f916f648a7c53b30f83f9fd5325
ПетуненковаАП <apetunenkovaa@gmail.com>
2022-09-14 16:19:12 +0300

    Work version

--- app/src/main/java/com/example/project_sql/ConnectionHelper.java
+++ app/src/main/java/com/example/project_sql/ConnectionHelper.java
@@ -1,15 +1,42 @@
 package com.example.project_sql;
 
-import android.telecom.Connection;
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ConnectionHelper {
 
     String userName,userPassword,ip,port,dataBase;
-
+    @SuppressLint("NewApi")
     public Connection connectionClass()
     {
         ip="ngknn.ru";
-        dataBase=""
+        dataBase="Petunenkova_Project_SQL";
+        userPassword="12357";
+        userName="31П";
+        port="1433";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Connection connection = null;
+        String connectionURL = null;
+
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + dataBase + ";user=" + userName + ";password=" + userPassword + ";";
+            connection = DriverManager.getConnection(connectionURL);
+
+        }
+        catch(Exception ex)
+        {
+        Log.e("Error",ex.getMessage());
+        }
+        return connection;
+
+
     }
 
 }
--- app/src/main/java/com/example/project_sql/MainActivity.java
+++ app/src/main/java/com/example/project_sql/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.project_sql;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+    Connection connection;
+    String ConnectionResult= "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void GetTextFromSql(View v){
+        TextView ID = findViewById(R.id.tv_ID);
+        TextView Manufacturer = findViewById(R.id.tv_Manufacturer);
+        TextView Model = findViewById(R.id.tv_Model);
+        TextView Storage = findViewById(R.id.tv_Storage);
+        TextView Color = findViewById(R.id.tv_Color);
+        TextView Cost = findViewById(R.id.tv_Cost);
+
+    try{
+        ConnectionHelper connectionHelper = new  ConnectionHelper();
+        connection = connectionHelper.connectionClass();
+
+        if(connection!=null)
+        {
+            String query = "Select * From [Mobile phone]";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next())
+            {
+                ID.setText(resultSet.getString(1));
+                Manufacturer.setText(resultSet.getString(2));
+                Model.setText(resultSet.getString(3));
+                Storage.setText(resultSet.getString(4));
+                Color.setText(resultSet.getString(5));
+                Cost.setText(resultSet.getString(6));
+            }
+
+        }
+        else
+        {
+            ConnectionResult = "Сheck Connection";
+        }
+    }
+    catch(Exception ex)
+    {
+        Log.e("Error",ex.getMessage());
+    }
     }
 }
