b9e663f4557be5093f79db1dab268252524d2315
Петуненкова Анастасия Павловна <@ngknn.local>
2022-09-14 13:49:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/project_sql/ConnectionHelper.java
@@ -0,0 +1,15 @@
+package com.example.project_sql;
+
+import android.telecom.Connection;
+
+public class ConnectionHelper {
+
+    String userName,userPassword,ip,port,dataBase;
+
+    public Connection connectionClass()
+    {
+        ip="ngknn.ru";
+        dataBase=""
+    }
+
+}
